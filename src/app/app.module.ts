af9d43ab269c7fc98be155b4abd262ca8338fed3
Abhishek <badhanabhishek3@gmail.com>
2021-01-14 16:51:22 +0530

    Initial git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './main/main.component';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatCardModule } from "@angular/material/card";
+import { MatRippleModule } from "@angular/material/core";
+import { ChartsModule } from 'ng2-charts';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatRippleModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,182 @@
+import { Component, OnInit } from '@angular/core';
+import { Chart } from "chart.js";
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  datas: object = [
+    
+    {
+      "_id": "5fffef6d033ed3e7fd0ad079",
+      "picture": "../../assets/company_logos/logo1.svg",
+      "name": "Cheri Roth",
+      "company": "MELBACOR",
+      "registered": "2018-08-17",
+      "location": "Succasunna"
+    },
+    {
+      "_id": "5fffef6d5cf13e10cf9030b8",
+      "picture": "../../assets/company_logos/logo2.svg",
+      "name": "Graciela Kirk",
+      "company": "UNEEQ",
+      "registered": "2017-10-06",
+      "location": "Bartonsville"
+    },
+    {
+      "_id": "5fffef6d3e88d7222640a833",
+      "picture": "../../assets/company_logos/logo3.svg",
+      "name": "Baird Roach",
+      "company": "STROZEN",
+      "registered": "2016-04-26",
+      "location": "Brandermill"
+    },
+    {
+      "_id": "5fffef6d94027a908e451891",
+      "picture": "../../assets/company_logos/logo4.svg",
+      "name": "Willie Obrien",
+      "company": "BITREX",
+      "registered": "2019-11-03",
+      "location": "Kenwood"
+    },
+    {
+      "_id": "5fffef6d2989889e8fa3e6a3",
+      "picture": "../../assets/company_logos/logo1.svg",
+      "name": "Buckner Strickland",
+      "company": "ZILLAN",
+      "registered": "2019-03-17",
+      "location": "Cliff"
+    },
+    {
+      "_id": "5fffef6d9c5b9d7d7f7b9518",
+      "picture": "../../assets/company_logos/logo2.svg",
+      "name": "Hanson Holloway",
+      "company": "RONBERT",
+      "registered": "2014-05-05",
+      "location": "Whitestone"
+    },
+    {
+      "_id": "5fffef6d23b6e4a08652ce17",
+      "picture": "../../assets/company_logos/logo3.svg",
+      "name": "Letha Frye",
+      "company": "ENTHAZE",
+      "registered": "2015-06-27",
+      "location": "Wolcott"
+    }
+    
+  ]
+
+  users: object = [
+    {
+      "_id": "60000524bcfb3e35b9d9a06e",
+      "picture": "http://placehold.it/32x32",
+      "name": "Velez Pittman",
+      "address": "Caron, Tennessee",
+      "salary": "$2519"
+    },
+    {
+      "_id": "6000052496cadc8adf17931e",
+      "picture": "http://placehold.it/32x32",
+      "name": "Reyna Burch",
+      "address": "Orviston, Colorado",
+      "salary": "$1491"
+    },
+    {
+      "_id": "600005249d0e18831b5168bd",
+      "picture": "http://placehold.it/32x32",
+      "name": "Hebert Lawson",
+      "address": "Gerber, Virgin Islands",
+      "salary": "$2994"
+    },
+    {
+      "_id": "60000524bc99c5344973c3d7",
+      "picture": "http://placehold.it/32x32",
+      "name": "Foreman Lucas",
+      "address": "Lloyd, New Jersey",
+      "salary": "$1097"
+    },
+    {
+      "_id": "600005248fd81fe0c38bbd01",
+      "picture": "http://placehold.it/32x32",
+      "name": "Witt Berry",
+      "address": "Grandview, Florida",
+      "salary": "$2229"
+    },
+    {
+      "_id": "60000524d3eb798a1ef78286",
+      "picture": "http://placehold.it/32x32",
+      "name": "Valdez Leon",
+      "address": "Freelandville, Ohio",
+      "salary": "$1731"
+    }
+  ]
+
+  
+  
+  constructor() { }
+
+  ngOnInit(): void {
+    var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var myChart = new Chart("chart", {
+			type: 'line',
+			data: {
+				labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+				datasets: [ {
+					label: 'My Second dataset',
+					fill: false,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        data: [12, 7, 3, 5, 3, 3, 5, 6, 3, 5, 2, 3],
+				}]
+			},
+			options: {
+				responsive: true,
+				// title: {
+				// 	display: true,
+				// 	text: 'Chart.js Line Chart'
+				// },
+				tooltips: {
+					mode: 'index',
+					intersect: false,
+				},
+				hover: {
+					mode: 'nearest',
+					intersect: true
+				},
+				scales: {
+					xAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Month'
+						}
+					}],
+					yAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Value'
+						}
+					}]
+				}
+			}
+		})
+  }
+}
